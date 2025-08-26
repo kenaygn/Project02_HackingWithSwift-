6e0a176c8659f0844a39c7116a176e798ebea426
Kenay <kenay@MacKenay.local>
2025-08-26 19:36:11 -0300

    Finish the project 2

--- Project02_HS/ViewController.swift
+++ Project02_HS/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var button3: UIButton!
     
     var countries: [String] = ["estonia", "france", "germany", "italy", "spain", "uk", "us", "russia", "poland", "nigeria", "monaco"]
+    
+    var correctAnswer: Int = 0
     var score: Int = 0
     
     override func viewDidLoad() {
@@ -23,7 +25,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction? = nil) {
+        countries.shuffle()//Vai fazer com que fique embaralhando a matriz
+        correctAnswer = Int.random(in: 0...2)
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
@@ -39,9 +44,28 @@ class ViewController: UIViewController {
         button1.backgroundColor = UIColor.lightGray
         button2.backgroundColor = UIColor.lightGray
         button3.backgroundColor = UIColor.lightGray
+        
+        title = countries[correctAnswer].uppercased()
     }
 
-   
+   //sender vai ser o botao que foi clicado
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        //Criar alerta para mostrar Score e se esta certo
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+        
+    }
     
 }
 
